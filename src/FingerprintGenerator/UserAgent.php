8da93a15cb550b8ae53e466666828869d081b1f4
ron <ron.kirschler@gmail.com>
2015-05-01 23:33:40 +0200

    Make User-Agent changeable

--- src/FingerprintGenerator/UserAgent.php
+++ src/FingerprintGenerator/UserAgent.php
@@ -3,12 +3,27 @@ namespace Sesshin\FingerprintGenerator;
 
 class UserAgent implements FingerprintGeneratorInterface
 {
+    /** @var string */
+    private $userAgent;
+
+    /**
+     * @param string $userAgent
+     */
+    public function __construct($userAgent = null)
+    {
+        if($userAgent) {
+            $srv = array_merge(array('HTTP_USER_AGENT' => ''), $_SERVER);
+            $userAgent = $srv['HTTP_USER_AGENT'];
+        }
+        $this->userAgent = $userAgent;
+    }
+
+
     /**
      * @return string
      */
     public function generate()
     {
-        $userAgent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
-        return sha1($userAgent);
+        return sha1($this->userAgent);
     }
 }
